c065c7f9761282f6699c02e3ad0c58b94a8b8900
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-11-11 12:30:57 -0700

    notes complete

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -12,13 +12,74 @@ than the graph's maximum degree.
 *Note: We can color a graph in linear time and space. Also, make sure that your
 solution can handle a loop in a reasonable way.*
 """
+
+colors = ['Yellow', 'Blue', 'Red', 'Green', 'Purple']
 # Definition for a graph node.
 class GraphNode:
-    def __init__(self, label):
-        self.label = label
+    def __init__(self, val):
+        self.val = val
         self.neighbors = set()
         self.color = None
 
 def color_graph(graph, colors):
     # Your code here
 
+    # 1 color every graph
+
+    # how do we min the # of colors used
+    # 1st node, any color is fine since none of the niehgbors have a color yet
+    # as we continue iterating through our graph, we need to figure out what colors are illegal for the given node
+        # we can iterate through the node's neighbors to see which ones are colored
+    # from there we can build up a set of illegal colors for each node
+
+
+
+    for node in graph:
+        illegal_colors = set()
+
+        for neighbor in node.neighbors:
+            if neighbor.color is not None:
+                illegal_colors.add(neighbor.color)
+                
+        # figure out which color(s) from the colors list is not in this illegal set of colors for a given node
+        # then assign its color to that color
+
+        for color in colors:
+            if color not in illegal_colors:
+                node.color = color
+                break
+
+# setting up our graph, see visual representation in notes from 11/11 in notepad channel
+g1 = GraphNode('G1')
+g2 = GraphNode('G2')
+g3 = GraphNode('G3')
+g4 = GraphNode('G4')
+g5 = GraphNode('G5')
+
+g1.neighbors.add(g2)
+g1.neighbors.add(g4)
+g1.neighbors.add(g3)
+
+g2.neighbors.add(g1)
+g2.neighbors.add(g4)
+g2.neighbors.add(g5)
+
+g3.neighbors.add(g1)
+g3.neighbors.add(g5)
+g3.neighbors.add(g4)
+
+g4.neighbors.add(g1)
+g4.neighbors.add(g2)
+g4.neighbors.add(g3)
+g4.neighbors.add(g5)
+
+g5.neighbors.add(g2)
+g5.neighbors.add(g3)
+g5.neighbors.add(g4)
+
+graph = [g1, g2, g3, g4, g5]
+
+color_graph(graph, colors)
+
+for node in graph:
+    print(node.color)
--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -29,4 +29,64 @@ from collections import deque
 
 def numIslands(grid):
     # Your code here
+    num_islands = 0
+    # iterate through our map 
+    for i, row in enumerate(grid):
+        for j, loc in enumerate(row):
+            # what do we do when we see a 1? 
+            # we need to figure out how large that island is 
+            # the island could just consist of a single 1, or
+            # it could have other 1's connected to it 
+            if loc == "1":
+                num_islands += 1
+                # when we encounter a 1, we need to traverse that island 
+                # by looking to the right and by looking down 
+                # we can use either a depth-first traversal or a breadth-first traversal
+                # doesn't matter which one we choose, since it doesn't matter the order
+                # in which we visit them 
+                stack = [(i, j)] 
+                # toggle this 1 to a 0
+                grid[i][j] = "0"
 
+                while len(stack) > 0:
+                    r, c = stack.pop()
+                    # look to the left
+                    if c > 0 and grid[r][c-1] == '1':
+                        # add that spot to our stack so that we'll visit it 
+                        stack.append((r, c - 1))
+                        grid[r][c-1] = '0' 
+
+                    # look to the right
+                    # we need to increment the col index by 1 
+                    # we also need to make sure that col + 1 is still in bounds 
+                    if c < len(row) - 1 and grid[r][c+1] == '1':
+                        # add that spot to our stack so that we'll visit it 
+                        stack.append((r, c + 1))
+                        grid[r][c+1] = '0'
+
+                    # look up
+                    if r > 0 and grid[r-1][c] == '1':
+                        # add that spot to our stack so that we'll visit it 
+                        stack.append((r - 1, c))
+                        grid[r-1][c] = '0' 
+
+                    # look down 
+                    if r < len(grid) - 1 and grid[r+1][c] == '1':
+                        # add that spot to our stack so that we'll visit it 
+                        stack.append((r + 1, c))
+                        grid[r+1][c] = '0' 
+
+            # over the course of traversing an island, how do we avoid
+            # double-counting 1's we've already seen before? 
+            # toggle any 1's we've accounted for to a 0 to avoid double-counting 
+            # keep a separate data structure that keeps track of the coordintes
+            # that we've already visited 
+
+    return num_islands
+
+grid = [
+  ['0', '1'],
+  ['1', '1']
+]
+
+print(numIslands(grid))
